de42a512244afc7be62dac8f497b54ef595aa0ef
Ivan Saldano <ivanms1@gmail.com>
2022-11-06 16:46:21 +0900

    feat: add plop js and clxs

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,17 @@
+import clsx from "clsx";
+
+import styles from "./Button.module.scss";
+
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
 }
 
-function Button({ children, ...props }: ButtonProps) {
-  return <button {...props}>{children}</button>;
+function Button({ className, children, ...props }: ButtonProps) {
+  return (
+    <button className={clsx(styles.Button, className)} {...props}>
+      {children}
+    </button>
+  );
 }
 
 export default Button;
